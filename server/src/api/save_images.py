0a474c5fbba93559ae20ef8a63ddd4bb5cb5c241
danielto1404 <daniilkorolev00@gmail.com>
2022-04-26 18:22:13 +0300

    -- update deps

--- server/src/api/save_images.py
+++ server/src/api/save_images.py
@@ -12,14 +12,14 @@ def resize(img, size=280):
     return img
 
 
-def embbed_image_in_box(img, box_size):
-    imgg = np.asarray(img, np.uint8)
+def embed_image_in_box(img, box_size):
+    image_copy = np.asarray(img, np.uint8)
     box = np.zeros((box_size, box_size, 3))
 
     half = box_size // 2
-    half_x, half_y = imgg.shape[0] // 2, imgg.shape[1] // 2
+    half_x, half_y = image_copy.shape[0] // 2, image_copy.shape[1] // 2
 
-    bound = imgg[:, :, :3]
+    bound = image_copy[:, :, :3]
 
     box[half - half_x: half + half_x, half - half_y: half + half_y, :] = bound
     return Image.fromarray(box.astype(np.uint8))
@@ -27,11 +27,11 @@ def embbed_image_in_box(img, box_size):
 
 def resize_source(img):
     img = resize(img, 360)
-    box = embbed_image_in_box(img, 361)
+    box = embed_image_in_box(img, 361)
     return box
 
 
 def resize_target(img):
     img = resize(img, 290)
-    box = embbed_image_in_box(img, 361)
+    box = embed_image_in_box(img, 361)
     return box
